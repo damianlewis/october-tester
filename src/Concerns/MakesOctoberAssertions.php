2006e8d453a4ad7eb0c45ef2ba7f0cf40087afa8
Damian Lewis <damian@damianlewis.co.uk>
2018-02-21 17:30:16 +0000

    Make October form widget and form group assertions

--- src/Concerns/MakesOctoberAssertions.php
+++ src/Concerns/MakesOctoberAssertions.php
@@ -8,6 +8,29 @@ use Facebook\WebDriver\Exception\NoSuchElementException;
 trait MakesOctoberAssertions
 {
     /**
+     * Assert that the form group with the given name is visible.
+     *
+     * @param string $name
+     *
+     * @return $this
+     */
+    public function assertFormGroupVisible($name)
+    {
+        return $this->assertFormElementVisible(".form-group[data-field-name='${name}']", 'Form field');
+    }
+
+    /**
+     * Assert that the form group with the given name is not on the page.
+     *
+     * @param string $name
+     *
+     * @return $this
+     */
+    public function assertFormGroupMissing($name)
+    {
+        return $this->assertFormElementMissing(".form-group[data-field-name='${name}']", 'form field');
+    }
+    /**
      * Assert that the form field with the given type and name is visible.
      *
      * @param string $type
@@ -17,15 +40,7 @@ trait MakesOctoberAssertions
      */
     public function assertFormFieldVisible($type, $name)
     {
-        $selector = ".${type}-field[data-field-name='${name}']";
-        $fullSelector = $this->resolver->format($selector);
-
-        PHPUnit::assertTrue(
-            $this->resolver->findOrFail($selector)->isDisplayed(),
-            "Form field [{$fullSelector}] is not visible."
-        );
-
-        return $this;
+        return $this->assertFormElementVisible(".${type}-field[data-field-name='${name}']", 'Form field');
     }
 
     /**
@@ -38,7 +53,65 @@ trait MakesOctoberAssertions
      */
     public function assertFormFieldMissing($type, $name)
     {
-        $selector = ".${type}-field[data-field-name='${name}']";
+        return $this->assertFormElementMissing(".${type}-field[data-field-name='${name}']", 'form field');
+    }
+
+    /**
+     * Assert that the form widget with the given type and name is visible.
+     *
+     * @param string $type
+     * @param string $name
+     *
+     * @return $this
+     */
+    public function assertFormWidgetVisible($type, $name)
+    {
+        return $this->assertFormElementVisible("[data-field-name='${name}'] [data-control='${type}']", 'Form widget');
+    }
+
+    /**
+     * Assert that the form widget with the given type and name is not on the page.
+     *
+     * @param string $type
+     * @param string $name
+     *
+     * @return $this
+     */
+    public function assertFormWidgetMissing($type, $name)
+    {
+        return $this->assertFormElementMissing("[data-field-name='${name}'] [data-control='${type}']", 'form widget');
+    }
+
+    /**
+     * Assert that the form element with the given selector is visible.
+     *
+     * @param string $selector
+     * @param string $messageFragment
+     *
+     * @return $this
+     */
+    public function assertFormElementVisible($selector, $messageFragment)
+    {
+        $fullSelector = $this->resolver->format($selector);
+
+        PHPUnit::assertTrue(
+            $this->resolver->findOrFail($selector)->isDisplayed(),
+            "${$messageFragment} [{$fullSelector}] is not visible."
+        );
+
+        return $this;
+    }
+
+    /**
+     * Assert that the element with the given selector is not on the page.
+     *
+     * @param string $selector
+     * @param string $messageFragment
+     *
+     * @return $this
+     */
+    public function assertFormElementMissing($selector, $messageFragment)
+    {
         $fullSelector = $this->resolver->format($selector);
 
         try {
@@ -47,7 +120,7 @@ trait MakesOctoberAssertions
             $missing = true;
         }
 
-        PHPUnit::assertTrue($missing, "Saw unexpected form field [{$fullSelector}].");
+        PHPUnit::assertTrue($missing, "Saw unexpected ${$messageFragment} [{$fullSelector}].");
 
         return $this;
     }
